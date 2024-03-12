93941833b6e470736c1d83c37cf712d04a12caa8
Cayden Andrews <144056074+D761454@users.noreply.github.com>
2024-03-12 16:49:55 +0000

    Update main.cpp

--- OpenGLT2/main.cpp
+++ OpenGLT2/main.cpp
@@ -1,7 +1,7 @@
 #include "HelloGL.h"
 
 int main(int argc, char* argv[]) {
-	HelloGL* game = new HelloGL(); // create intance of the game
+	HelloGL* game = new HelloGL(argc, argv); // create intance of the game
 
 	return 0; // assume successful game exit
 }
