ac7cdd954593a44c98beeca5b35967b9e32f6662
Cayden Andrews <144056074+D761454@users.noreply.github.com>
2024-03-12 16:44:01 +0000

    f

--- OpenGLT2/HelloGL.cpp
+++ OpenGLT2/HelloGL.cpp
@@ -1,1 +1,8 @@
 #include "HelloGL.h"
+
+HelloGL::HelloGL(int argc, char* argv[]) {
+	glutInit(&argc, argv);
+	glutCreateWindow("Simple OpenGL Program");
+	glutDisplayFunc(Display);
+	glutMainLoop();
+}
--- OpenGLT2/HelloGL.h
+++ OpenGLT2/HelloGL.h
@@ -1,4 +1,9 @@
 #pragma once
+#include <Windows.h> // Req
+#include <gl/GL.h> // OpenGl
+#include <gl/GLU.h> // OpenGL Utilities
+#include "GL\freeglut.h" // freeglut library
+
 class HelloGL
 {
 };
