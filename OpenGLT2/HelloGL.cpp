d9bac2df6d1408b4b5657a0f71af2155f40e2941
Cayden Andrews <144056074+D761454@users.noreply.github.com>
2024-03-14 09:19:26 +0000

    Update HelloGL.cpp

--- OpenGLT2/HelloGL.cpp
+++ OpenGLT2/HelloGL.cpp
@@ -10,7 +10,16 @@ HelloGL::HelloGL(int argc, char* argv[]) {
 }
 
 void HelloGL::Display() {
-
+	// drawing code
+	glClear(GL_COLOR_BUFFER_BIT); // clear scene
+	glBegin(GL_POLYGON); // begin drawing
+	glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+		glVertex2f(-0.75, 0.5); // top left
+		glVertex2f(0.75, 0.5); // top right
+		glVertex2f(0.75, -0.5); // bottom right
+		glVertex2f(-0.75, -0.5); // bottom left
+	glEnd(); // end drawing
+	glFlush(); // flush scene to graphics card
 }
 
 HelloGL::~HelloGL(void) {
