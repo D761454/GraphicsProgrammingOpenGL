d74dbb55d6398249b3253f9f9bbd59764ed23f22
D761454 <144056074+D761454@users.noreply.github.com>
2024-04-08 12:48:45 +0100

    Update HelloGL.cpp

--- OpenGLT2/HelloGL.cpp
+++ OpenGLT2/HelloGL.cpp
@@ -3,7 +3,7 @@
 HelloGL::HelloGL(int argc, char* argv[]) {
 	// camera setup
 	camera = new Camera();
-	camera->eye.x = 0.0f; camera->eye.y = 0.0f; camera->eye.z = -5.0f;
+	camera->eye.x = 0.0f; camera->eye.y = 0.0f; camera->eye.z = 50.0f;
 	camera->center.x = 0.0f; camera->center.y = 0.0f; camera->center.z = 0.0f;
 	camera->up.x = 0.0f; camera->up.y = 1.0f; camera->up.z = 0.0f;
 
