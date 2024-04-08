bd3c6a29289b5d153df05bef7b7606b7432d35a8
D761454 <144056074+D761454@users.noreply.github.com>
2024-04-08 11:03:31 +0100

    struct header file

--- OpenGLT2/HelloGL.h
+++ OpenGLT2/HelloGL.h
@@ -1,31 +1,16 @@
 #pragma once
 #include <Windows.h> // Req
-#include <gl/GL.h> // OpenGl
-#include <gl/GLU.h> // OpenGL Utilities
-#include "GL\freeglut.h" // freeglut library
+//#include <gl/GL.h> // OpenGl
+//#include <gl/GLU.h> // OpenGL Utilities
+//#include "GL\freeglut.h" // freeglut library
 #include "GLUTCallbacks.h"
 #include <iostream>
 #include <fstream>
+#include "Structures.h"
 
 using namespace std;
 
 #define REFRESHRATE 16
-
-struct Vector3 {
-	float x, y, z;
-};
-
-struct Camera {
-	Vector3 eye, center, up;
-};
-
-struct Color {
-	GLfloat r, g, b, a;
-};
-
-struct Vertex {
-	GLfloat x, y, z;
-};
 
 class HelloGL
 {
---
+++ OpenGLT2/Structures.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <gl/GL.h> // OpenGl
+#include <gl/GLU.h> // OpenGL Utilities
+#include "GL\freeglut.h" // freeglut library
+
+
+struct Vector3 {
+	float x, y, z;
+};
+
+struct Camera {
+	Vector3 eye, center, up;
+};
+
+struct Color {
+	GLfloat r, g, b, a;
+};
+
+struct Vertex {
+	GLfloat x, y, z;
+};
