cda6ca23b2f968999db856c25cdd79bb45ddbbb9
Cayden Andrews <144056074+D761454@users.noreply.github.com>
2024-04-30 15:03:43 +0100

    Update Structures.h

--- OpenGLT2/Structures.h
+++ OpenGLT2/Structures.h
@@ -9,6 +9,19 @@ struct Vector3 {
 	float x, y, z;
 };
 
+struct Vector4 {
+	float x, y, z, w;
+};
+
+struct Lighting {
+	Vector4 ambient, diffuse, specular;
+};
+
+struct Material {
+	Vector4 ambient, diffuse, specular;
+	GLfloat shininess;
+};
+
 struct Camera {
 	Vector3 eye, center, up;
 	float angleX, angleY, radius;
